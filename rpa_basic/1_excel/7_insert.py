6581821053f33ddb5d9b34e66e465e5333056fdb
Wonhee Yun <56327729+wonhee3472@users.noreply.github.com>
2022-06-16 11:07:00 -0500

    learned how to insert empty rows and cols

--- rpa_basic/1_excel/7_insert.py
+++ rpa_basic/1_excel/7_insert.py
@@ -2,9 +2,9 @@ from openpyxl import load_workbook
 wb = load_workbook("sample.xlsx")
 ws = wb.active
 
-# ws.insert_rows(8) # emptying the 8th row
+# ws.insert_rows(8)  # emptying the 8th row
 # ws.insert_rows(8, 5)  # inserting 5 rows at the 8th row
 # wb.save('sample_insert_rows.xlsx')
 
-ws.insert_cols(2)
+ws.insert_cols(2, 5)
 wb.save("sample_insert_columns.xlsx")
